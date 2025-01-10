67bc578979cdcc53bc58f6f5b04ff3cab82e3a1a
MILAN DJORDJEVIC <milandjordjevic633@gmail.com>
2025-01-10 09:40:38 +0100

    initital

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,14 +3,19 @@ import { Auth, getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseApiKey = process.env.FIREBASE_API_KEY;
+const firebaseAuthDomain = process.env.FIREBASE_AUTH_DOMAIN;
+const firebaseProjectId = process.env.FIREBASE_PROJECT_ID;
+const firebaseStorageBucket = process.env.FIREBASE_STORAGE_BUCKET;
+const firebaseMessagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID;
+const firebaseAppId = process.env.FIREBASE_APP_ID;
 
 const firebaseConfig = {
   apiKey: firebaseApiKey,
-  authDomain: "milchat-82eed.firebaseapp.com",
-  projectId: "milchat-82eed",
-  storageBucket: "milchat-82eed.firebasestorage.app",
-  messagingSenderId: "181540813779",
-  appId: "1:181540813779:web:00b962f79f27fa6e7c8d26",
+  authDomain: firebaseAuthDomain,
+  projectId: firebaseProjectId,
+  storageBucket: firebaseStorageBucket,
+  messagingSenderId: firebaseMessagingSenderId,
+  appId: firebaseAppId,
 };
 
 const app = initializeApp(firebaseConfig);
