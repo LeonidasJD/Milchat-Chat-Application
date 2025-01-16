1169a10d600d9f77f07ebcfb83ef08152354ccb2
MILAN DJORDJEVIC <milandjordjevic633@gmail.com>
2025-01-16 22:32:36 +0100

    init

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { Auth, getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseApiKey = process.env.FIREBASE_API_KEY;
-const firebaseAuthDomain = process.env.FIREBASE_AUTH_DOMAIN;
-const firebaseProjectId = process.env.FIREBASE_PROJECT_ID;
-const firebaseStorageBucket = process.env.FIREBASE_STORAGE_BUCKET;
-const firebaseMessagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID;
-const firebaseAppId = process.env.FIREBASE_APP_ID;
+const firebaseApiKey = import.meta.env.VITE_FIREBASE_API_KEY;
+const firebaseAuthDomain = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+const firebaseProjectId = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+const firebaseStorageBucket = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
+const firebaseMessagingSenderId = import.meta.env
+  .VITE_FIREBASE_MESSAGING_SENDER_ID;
+const firebaseAppId = import.meta.env.VITE_FIREBASE_APP_ID;
 
 const firebaseConfig = {
   apiKey: firebaseApiKey,
