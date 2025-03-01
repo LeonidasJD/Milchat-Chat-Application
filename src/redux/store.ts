6cfc4e952c86d835835a755736ee1895ffd24eb4
LeonidasJD <milandjordjevic633@gmail.com>
2025-03-01 18:55:58 +0100

    changed store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slice/userSlice";
 import aboutUsReducer from "./slice/aboutUsSlice";
+import selectedUserReducer from "./slice/selectedUserSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     aboutUsText: aboutUsReducer,
+    setSelectedUserData: selectedUserReducer,
   },
 });
 
