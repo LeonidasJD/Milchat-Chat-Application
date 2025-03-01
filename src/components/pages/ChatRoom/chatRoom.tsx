20d9308889ce6c3a63a031a463b6588c5f1086c0
LeonidasJD <milandjordjevic633@gmail.com>
2025-03-01 18:55:51 +0100

    created logic for separate chats

--- src/components/pages/ChatRoom/chatRoom.tsx
+++ src/components/pages/ChatRoom/chatRoom.tsx
@@ -13,41 +13,62 @@ import "./chatRoom.scss";
 import { Button, Input } from "antd";
 import { SendOutlined } from "@ant-design/icons";
 import UserList from "../../common/users/users";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../redux/store";
 
 const ChatRoom = () => {
   interface Message {
     id: string;
     text: string;
     senderId: string;
+    receiverId: string;
     createdAt: any;
   }
   const [messages, setMessages] = useState<Message[]>([]);
   const [newMessage, setNewMessage] = useState("");
+  const selectedUserId = useSelector(
+    (state: RootState) => state.setSelectedUserData.userId
+  );
+  const selectedUserName = useSelector(
+    (state: RootState) => state.setSelectedUserData.userName
+  );
 
   // HOOK FOR FETCHING MESSAGES FROM DATABASE
   useEffect(() => {
+    if (!selectedUserId) return;
+
     const q = query(collection(db, "messages"), orderBy("createdAt"));
     const unsubscribe = onSnapshot(q, (snapshot) => {
-      const msgs = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        text: doc.data().text,
-        senderId: doc.data().senderId,
-        createdAt: doc.data().createdAt,
-      }));
+      const msgs = snapshot.docs
+        .map((doc) => ({
+          id: doc.id,
+          text: doc.data().text,
+          senderId: doc.data().senderId,
+          receiverId: doc.data().receiverId,
+          createdAt: doc.data().createdAt,
+        }))
+        .filter(
+          (message) =>
+            (message.senderId === auth?.currentUser?.uid &&
+              message.receiverId === selectedUserId) ||
+            (message.senderId === selectedUserId &&
+              message.receiverId === auth?.currentUser?.uid)
+        );
       setMessages(msgs);
     });
 
     return () => unsubscribe();
-  }, []);
+  }, [selectedUserId]);
 
   // FUNCTION FOR SENDING MESSAGES
 
   const sendMessage = async () => {
-    if (newMessage.trim() === "") return;
+    if (newMessage.trim() === "" || !selectedUserId) return;
     try {
       await addDoc(collection(db, "messages"), {
         text: newMessage,
         senderId: auth?.currentUser?.uid,
+        receiverId: selectedUserId,
         createdAt: serverTimestamp(),
       });
       setNewMessage("");
@@ -67,53 +88,58 @@ const ChatRoom = () => {
         <div className="users-wrapper">
           <UserList></UserList>
         </div>
-        <div className="messages-wrapper">
-          {messages.map((message) => (
-            <div
-              key={message.id}
-              style={{
-                margin: "10px 0",
-                textAlign:
-                  message.senderId === auth?.currentUser?.uid
-                    ? "right"
-                    : "left",
-              }}
-            >
-              <span
+        <div className="allChat">
+          <div className="receiverDataBar">
+            <h3>{selectedUserName}</h3>
+          </div>
+          <div className="messages-wrapper">
+            {messages.map((message) => (
+              <div
+                key={message.id}
                 style={{
-                  backgroundColor:
+                  margin: "10px 0",
+                  textAlign:
                     message.senderId === auth?.currentUser?.uid
-                      ? "#dcf8c6"
-                      : "#2e1b3e",
-                  padding: "8px",
-                  borderRadius: "10px",
-                  color:
-                    message.senderId === auth?.currentUser?.uid
-                      ? "black"
-                      : "white",
+                      ? "right"
+                      : "left",
                 }}
               >
-                {message.text}
-              </span>
-            </div>
-          ))}
-          <div className="input-wrapper">
-            <Input
-              className="sendMessageInput"
-              type="text"
-              value={newMessage}
-              onChange={(e) => setNewMessage(e.target.value)}
-              placeholder="Type a message..."
-            />
+                <span
+                  style={{
+                    backgroundColor:
+                      message.senderId === auth?.currentUser?.uid
+                        ? "#dcf8c6"
+                        : "#2e1b3e",
+                    padding: "8px",
+                    borderRadius: "10px",
+                    color:
+                      message.senderId === auth?.currentUser?.uid
+                        ? "black"
+                        : "white",
+                  }}
+                >
+                  {message.text}
+                </span>
+              </div>
+            ))}
+            <div className="input-wrapper">
+              <Input
+                className="sendMessageInput"
+                type="text"
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+                placeholder="Type a message..."
+              />
 
-            <Button
-              type="primary"
-              shape="round"
-              icon={<SendOutlined />}
-              size={"large"}
-              style={{ backgroundColor: "#2e1b3e", marginTop: "15px" }}
-              onClick={sendMessage}
-            />
+              <Button
+                type="primary"
+                shape="round"
+                icon={<SendOutlined />}
+                size={"large"}
+                style={{ backgroundColor: "#2e1b3e", marginTop: "15px" }}
+                onClick={sendMessage}
+              />
+            </div>
           </div>
         </div>
       </div>
