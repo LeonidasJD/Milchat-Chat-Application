a909f27ff94660f1b8e600049b09247ae1de799d
LeonidasJD <milandjordjevic633@gmail.com>
2025-04-03 20:34:09 +0200

    status update

--- src/hooks/useUserStatus.ts
+++ src/hooks/useUserStatus.ts
@@ -1,5 +1,11 @@
 import { useEffect } from "react";
-import { ref, onDisconnect, set, serverTimestamp } from "firebase/database";
+import {
+  ref,
+  onDisconnect,
+  set,
+  serverTimestamp,
+  onValue,
+} from "firebase/database";
 import { auth, realtimeDb } from "../firebase/firebase"; // Importuj realTimeDB iz firebase config-a
 import { onAuthStateChanged } from "firebase/auth";
 
@@ -8,23 +14,19 @@ const useUserStatus = () => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       const userStatusRef = ref(realtimeDb, `/status/${user?.uid}`);
 
-      if (!user) {
-        // Ako nema korisnika (logout), postavi status na "offline"
-        set(userStatusRef, {
-          state: "offline",
-          lastChanged: serverTimestamp(),
-        });
-      } else {
-        // Ako korisnik postoji, postavi status na "online"
-        set(userStatusRef, {
-          state: "online",
-          lastChanged: serverTimestamp(),
-        });
-      }
+      const connectionRef = ref(realtimeDb, ".info/connected");
+      onValue(connectionRef, (snapshot) => {
+        if (snapshot.val() === true) {
+          set(userStatusRef, {
+            state: "connected",
+            lastChanged: serverTimestamp(),
+          });
+        }
+      });
 
       // Postavi da bude offline kad se korisnik diskonektuje
       onDisconnect(userStatusRef).set({
-        state: "offline",
+        state: "disconnected",
         lastChanged: serverTimestamp(),
       });
     });
